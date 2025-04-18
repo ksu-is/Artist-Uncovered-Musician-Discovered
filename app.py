7d18811e8e001b026f65dfb98bad87bb93b6a44f
wbatessk <wbatessk@students.kennesaw.edu>
2025-04-18 18:12:21 -0400

    Update app.py
    Created app.py with basic Flask setup and home route

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# Set up basic Flask app
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -6,11 +5,6 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('home.html')
-
-@app.route('/search', methods=['POST'])
-def search():
-    artist = request.form['artist']
-    return render_template('results.html', artist=artist)
 
 if __name__ == '__main__':
     app.run(debug=True)
