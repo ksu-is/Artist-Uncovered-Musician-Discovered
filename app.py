d4d860d25f444f98b6ccc488e772ff14b3eb1ce5
wbatessk <wbatessk@students.kennesaw.edu>
2025-04-21 20:19:34 -0400

    Update app.py
    Updated app.py to connect search with Spotify API and display result

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template, request
+from spotify_basic import search_artist
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/search', methods=['POST'])
+def search():
+    artist_name = request.form['artist']
+    result = search_artist(artist_name)
+    return f"Top result: {result}" 
 
 if __name__ == '__main__':
     app.run(debug=True)
